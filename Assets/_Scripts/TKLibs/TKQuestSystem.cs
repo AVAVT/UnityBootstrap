3d7ce0d38143ee78ff24461284ba2c30f85d8fab
huynq <qhuydtvt@gmail.com>
2017-04-20 05:56:07 +0700

    Added quest system

---
+++ Assets/_Scripts/TKLibs/TKQuestSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TKQuestSystem : MonoBehaviour {
+
+	private static TKQuestSystem instance;
+	
+	public void QuestCompleted(string questName) {
+		foreach (TKQuestUnit questUnit in GetComponents<TKQuestUnit>()) {
+			questUnit.QuestCompleted (questName);
+		}
+
+		print (questName);
+	}
+
+	private void Start() {
+		instance = this;
+	}
+
+	public static TKQuestSystem getInstance() {
+		return instance;
+	}
+}
